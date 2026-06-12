408ffd8e0e5c4a2d3e278c4b265152ffbb75205a
Denis Shmegelskiy <denissmegelskij6@gmail.com>
2026-06-12 18:34:39 +0300

    Component styling has been added

---
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,58 @@
+import Field from "../Field/Field.jsx";
+import Button from "../Button/Button.jsx";
+import {useContext, useState} from "react";
+import {TasksContext} from "../../context/TasksContext.jsx";
+
+const AddTaskForm = (props) => {
+  const {styles} = props
+  const {
+    addTask,
+    newTaskTitle,
+    setNewTaskTitle,
+    newTaskInputRef,
+
+  } = useContext(TasksContext)
+  const [error, setError] = useState('')
+  const clearNewTaskTitle = newTaskTitle.trim()
+  const isNewTaskTitleEmpty =
+    clearNewTaskTitle.length === 0
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (!isNewTaskTitleEmpty) {
+      addTask(clearNewTaskTitle)
+    }
+
+  }
+  const onInput = (e) => {
+    const {value} = e.target
+    const clearValue = value.trim()
+    const hasOnlySpaces =
+      value.length > 0 && clearValue.length === 0
+    setNewTaskTitle(value)
+    setError(hasOnlySpaces ? "The task cannot be empty" : '')
+  }
+  return (
+    <form
+      className={styles.form}
+      onSubmit={onSubmit}
+    >
+      <Field
+        className={styles.field}
+        label="New task title"
+        id="new-task"
+        value={newTaskTitle}
+        error={error}
+        onInput={onInput}
+        ref={newTaskInputRef}
+      />
+      <Button
+        type="submit"
+        isDisabled={isNewTaskTitleEmpty}
+      >
+        Add
+      </Button>
+    </form>
+  )
+}
+export default AddTaskForm
